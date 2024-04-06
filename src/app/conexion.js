7f4c499a0cab536ae8a6b4cd29e31f28d653da85
Orlandoseoanes <oaseoanes@unicesar.edu.co>
2024-04-06 16:26:57 -0500

    a5

--- src/app/conexion.js
+++ src/app/conexion.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const dotenv=require("dotenv");
-dotenv.config();
-// Set up dotenv with .env file
-console.log(process.env.DB_DATABASE);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_HOST);
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(preicfes, prueba,tMeque+2023+ {
+    host:srv435312.hstgr.cloud,
     dialect: "mysql",
 });
 
